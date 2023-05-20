51036aeedddccebd89e4b2f4c113de2967ddb8bf
ChristianWestgaard <91060882+ChristianWestgaard@users.noreply.github.com>
2023-05-20 15:27:15 +0200

    fix

--- router/router.js
+++ router/router.js
@@ -3,15 +3,16 @@ const Controller = require("../controllers/controller"); //exports our controlle
 const router  = Router(); ///creates a new router
 
 router.get("/", Controller.index_get);
+// router.post("/", Controller.index_post);
 
 // router.get("/signup", Controller.signup_get)
 // router.post("/signup", Controller.signup_post)
 
-// router.get("/logInn", Controller.logInn_get)
-// router.post("/logInn", Controller.logInn_post)
+router.get("/logInn", Controller.logInn_get)
+router.post("/logInn", Controller.logInn_post)
 
  router.get("/nyPokeSide", Controller.nyPokeSide_get)
- router.post("/nyPokeSide", Controller.nyPokeSide_post)
+  router.post("/nyPokeSide", Controller.nyPokeSide_post)
 
 // router.get("/minPokeSide", Controller.minPokeSide_get)
 // router.delete("/minPokeSide", Controller.minPokeSide_delete)
